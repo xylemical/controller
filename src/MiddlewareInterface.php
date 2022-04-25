71e26202d594c386d12738c0176f2d3705613104
Rhys McGuckin <rhysmcguckin@yahoo.com.au>
2022-04-26 09:12:42 +1000

    Remove composer.lock, add MiddlewareInterface.

---
+++ src/MiddlewareInterface.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Xylemical\Controller;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Process middleware that surrounds the request/response process.
+ */
+interface MiddlewareInterface {
+
+  /**
+   * The priority of the middleware.
+   *
+   * @return int
+   *   The priority.
+   */
+  public function priority(): int;
+
+  /**
+   * Process the request.
+   *
+   * @param \Xylemical\Controller\Controller $controller
+   *   The controller.
+   * @param \Psr\Http\Message\RequestInterface $request
+   *   The request.
+   * @param \Xylemical\Controller\ContextInterface $context
+   *   The context.
+   *
+   * @return \Psr\Http\Message\RequestInterface
+   *   The request.
+   *
+   * @throws \Throwable
+   */
+  public function request(Controller $controller, RequestInterface $request, ContextInterface $context): RequestInterface;
+
+  /**
+   * Process the response.
+   *
+   * @param \Xylemical\Controller\Controller $controller
+   *   The controller.
+   * @param \Psr\Http\Message\ResponseInterface $response
+   *   The response.
+   * @param \Xylemical\Controller\ContextInterface $context
+   *   The context.
+   *
+   * @return \Psr\Http\Message\ResponseInterface
+   *   The updated response.
+   *
+   * @throws \Throwable
+   */
+  public function response(Controller $controller, ResponseInterface $response, ContextInterface $context): ResponseInterface;
+
+}
